ace3c081dfe0e56932cb7e4b789d8c6abfc07182
dostiygrace <51546658+dostiygrace@users.noreply.github.com>
2019-07-11 18:52:19 +0100

    First commit

--- lovecalculator.php
+++ lovecalculator.php
@@ -10,21 +10,27 @@
  <fieldset>
  	<legend>LOVE CALCULATOR</legend>
  	<form>
+	   <div>
  		<div class="first">
  			<label>Your Name</label>
  			<input type="text"><br>
  			<input type="radio" name="radio">Male
  			<input type="radio" name="radio">Female
- 		</div>
- 		<div>
+			
+			 <p style="text-align:center;">
+
  			<div class="second">
  				<label>Your Lover's Name</label>
  				<input type="text"><br>
  				<input type="radio" name="radio">Male
- 				<input type="radio" name="radio">Female
+				 <input type="radio" name="radio">Female
+				 </p>
  			</div>
 
- 			<input type="submit">
+			 <input type="submit">
+			 <div style="position:relative;">
+			 	<img src="https://cdn.pixabay.com/photo/2016/11/19/12/25/art-1839006__340.jpg" width="300px" style="right:0; margin-top:-70px; position:absolute;" alt="">
+			 </div>
  </fieldset>
 </p>
 	 
