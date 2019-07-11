4cea205cea83a02ef0bc11cef9779ec409c1bf9f
dostiygrace <51546658+dostiygrace@users.noreply.github.com>
2019-07-11 13:06:32 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<?php
+
+$firstname = "Titilayo";
+$Surname =  "Dosa";
+$sistername = "comfort";
+$brothername = "john";
+$dob = "13 july";
+
+ ?>
+<style>
+ul{
+	list-style: none;
+}
+</style>
+
+ <html>
+
+ <head>
+<title>the monthly allowance</title>
+ </head>
+ <body>
+ 	
+  <ul>
+  <li>Firstname : <?php echo $firstname;?><li>
+  <li>Surname : <?php echo $Surname;?><li>
+  <li>Sistername : <?php echo $sistername;?><li>
+  <li>Brothername : <?php echo $brothername;?><li>
+  <li>Dob : <?php echo $dob;?><li></ul>
+  
+  
+  
+ </body>
+ </html>	
---
+++ lovecalculator.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>NAIJA LOVE CALCULATOR</title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+	 <h1>Calculate love compatibility and your chances of a successful relationship.</h1>
+<div class="form">
+ <fieldset>
+ 	<legend>LOVE CALCULATOR</legend>
+ 	<form>
+ 		<div class="first">
+ 			<label>Your Name</label>
+ 			<input type="text"><br>
+ 			<input type="radio" name="radio">Male
+ 			<input type="radio" name="radio">Female
+ 		</div>
+ 		<div>
+ 			<div class="second">
+ 				<label>Your Lover's Name</label>
+ 				<input type="text"><br>
+ 				<input type="radio" name="radio">Male
+ 				<input type="radio" name="radio">Female
+ 			</div>
+
+ 			<input type="submit">
+ </fieldset>
+</p>
+	 
+
+</div>
+
+	 
+
+</body>
+</html>
